0780e6b0152aee70e083d2684b1d7e64d348e597
Antonio Zea Jr <halosprtn117@gmail.com>
2021-07-07 15:02:50 -0400

    removed redundant variables

--- fibonacci/src/main.rs
+++ fibonacci/src/main.rs
@@ -2,19 +2,17 @@ fn main() {
     fibonacci(0, 1, 6);
 }
 
-
-fn fibonacci(first: i32, second:i32, mut n: i32) {
-    let mut p1 : i32 = first;
-    let mut p2 : i32 = second;
+// Program computes fibonacci sequence with arbitrary seed values first and second
+fn fibonacci(mut first: i32, mut second:i32, mut n: i32) {
     let mut next : i32;
     
-    println!("{}", p1);
+    println!("{}", first);
 
     while n > 0 {
-        next = p1 + p2;
+        next = first + second;
         println!("{}", next);
-        p2 = p1;
-        p1 = next;
+        second = first;
+        first = next;
         n -= 1;
     }
 }
