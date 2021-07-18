02708e1b7685a055644bec7cf2230b599ad9d994
Antonio Zea Jr <halosprtn117@gmail.com>
2021-07-18 08:51:23 -0400

    Added derived trait to Rectangle struct

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -1,10 +1,11 @@
+#[derive(Debug)]
 struct Rectangle {
     width: u32,
     height: u32,
 }
 
 fn main() {
-    let rect1 = Rectangle{
+    let rect1 = Rectangle {
         width: 30,
         height: 50,
     };
@@ -13,6 +14,8 @@ fn main() {
         "The area of the rectangle is {} square pixels.",
         area(&rect1)
     );
+
+    println!("rect1 is {:#?}", rect1);
 }
 
 fn area(rectangle: &Rectangle) -> u32 {
